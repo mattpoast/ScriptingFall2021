d40aaf2fa8bd85b8181b9bdc259dcbd7422b0a11
matt poast <matt.poast@gmail.com>
2021-09-01 11:25:14 -0600

    DataBasics

--- 3DBoxGame/Assets/Scripts/FloatData.cs
+++ 3DBoxGame/Assets/Scripts/FloatData.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 [CreateAssetMenu]
 public class FloatData : ScriptableObject
 {
-   
   public float value;
-  
   public void AddToValue(float num)
   {
     value += num;
+  }
+  public void ResetValue(float num)
+  {
+    value = num;
   }
 }
---
+++ 3DBoxGame/Assets/Scripts/MonoEventsBehavior.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MonoEventsBehavior : MonoBehaviour
+{
+    public UnityEvent startEvent;
+    void Start()
+    {
+        startEvent.Invoke();
+    }
+}
