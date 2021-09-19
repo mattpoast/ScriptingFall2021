cd68966c2095af5c3b5bad5064721e61efccb0bb
matt poast <matt.poast@gmail.com>
2021-09-19 14:14:31 -0600

    FingerDragMovementScript

---
+++ GhostMobile/Assets/Scripts/MobileDragMovement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+public class MobileDragMovement : MonoBehaviour
+{
+    public float speed = 0.3f;
+    public Vector3 touchPosition;
+    private void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchPosition = Input.GetTouch(0).deltaPosition;
+            transform.Translate(touchPosition.x * speed * Time.deltaTime, 0, touchPosition.z * speed * Time.deltaTime);
+        }
+    }
+}
