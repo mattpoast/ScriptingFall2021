b4f0b87c8faecefce7d030c7255f225c37f63a52
matt poast <matt.poast@gmail.com>
2021-09-20 21:15:41 -0600

    Added Ghost Model and animation

--- GhostMobile/Assets/Scripts/MobileDragMovement.cs
+++ GhostMobile/Assets/Scripts/MobileDragMovement.cs
@@ -3,12 +3,14 @@ public class MobileDragMovement : MonoBehaviour
 {
     public float speed = 0.3f;
     public Vector3 touchPosition;
+    public Animator animator;
     private void Update()
     {
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             touchPosition = Input.GetTouch(0).deltaPosition;
             transform.Translate(touchPosition.x * speed * Time.deltaTime, 0, touchPosition.z * speed * Time.deltaTime);
+            
         }
     }
 }
