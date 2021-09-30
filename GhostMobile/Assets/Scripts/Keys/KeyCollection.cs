148b9f834a7c713ec57bc3faa99fc93f4cfb192e
matt poast <matt.poast@gmail.com>
2021-09-30 12:15:14 -0600

    Script to change ui images based on collectingItems

--- GhostMobile/Assets/Scripts/Keys/KeyCollection.cs
+++ GhostMobile/Assets/Scripts/Keys/KeyCollection.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 public class KeyCollection : MonoBehaviour
 {
+    public Image key1Color, key2Color, key3Color;
+    public void Start()
+    {
+        key1Color.enabled = false;
+        key2Color.enabled = false;
+        key3Color.enabled = false;
+    }
     public void OnTriggerEnter(Collider other)
     {
         KeyList.keyCount += 1;
-        print(KeyList.keyCount);
+        if (KeyList.keyCount == 1)
+        {
+            key1Color.enabled = true;
+        }
+        if (KeyList.keyCount == 2)
+        {
+            key2Color.enabled = true;
+        }
+        if (KeyList.keyCount == 3)
+        {
+            key3Color.enabled = true;
+        }
         Destroy(gameObject);
     }
 }
