b3c8b7356c413730ddabadb4eb8197aa40f9173d
matt poast <matt.poast@gmail.com>
2021-10-25 10:15:11 -0600

    Coffin asset and script

---
+++ GhostMobile/Assets/Scripts/Coffin.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+public class Coffin : MonoBehaviour
+{
+    public Animator animator;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (KeyList.keyCount == 0)
+        {
+            animator.SetBool("KeyLocked" ,true);
+        }
+        if (KeyList.keyCount >= 1)
+        {
+            animator.SetBool("KeyOne", true);
+        }
+        if (KeyList.keyCount >= 2)
+        {
+            animator.SetBool("KeyTwo", true);
+        }
+        if (KeyList.keyCount == 3)
+        {
+            animator.SetBool("KeyThree", true);
+        }
+    }
+}
