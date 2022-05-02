a36814271fce66a501fe749176df400b07daa44c
matt poast <matt.poast@gmail.com>
2022-05-01 21:05:09 -0600

    SecondLevel, bug fixes

--- Cat Game/Assets/Scripts/OutOfBounds.cs
+++ Cat Game/Assets/Scripts/OutOfBounds.cs
@@ -3,7 +3,6 @@ public class OutOfBounds : MonoBehaviour
 {
     public void OnTriggerEnter2D(Collider2D other)
     {
-        print("Triggered");
         if (other.gameObject.tag == "Mouse")
         {
             Destroy(other.gameObject);
